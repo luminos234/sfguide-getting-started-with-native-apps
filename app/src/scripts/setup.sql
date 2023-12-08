31dea16b44d73e9eda749f22d36a6c6e70d6cb75
luminos234 <jan.zaluska@gmail.com>
2023-12-08 15:38:59 +0100

    Update setup.sql

--- app/src/scripts/setup.sql
+++ app/src/scripts/setup.sql
@@ -29,14 +29,6 @@ packages = ('snowflake-snowpark-python','pandas','scikit-learn==1.1.1')
 imports = ('/libraries/udf.py')
 handler = 'udf.cal_distance';
 
-create or replace function app_instance_schema.hello_world()
-returns string
-language python
-runtime_version = '3.8'
-packages = ('snowflake-snowpark-python')
-imports = ('/libraries/udf.py')
-handler = 'udf.hello_world';
-
 -- Create Stored Procedure
 create or replace procedure app_instance_schema.billing_event(number_of_rows int)
 returns string
@@ -65,6 +57,14 @@ begin
 end;
 $$;
 
+create or replace function app_instance_schema.hello_world()
+returns string
+language python
+runtime_version = '3.8'
+packages = ('snowflake-snowpark-python')
+imports = ('/libraries/udf.py')
+handler = 'udf.hello_world';
+
 -- Grant usage and permissions on objects
 grant usage on schema app_instance_schema to application role app_instance_role;
 grant usage on function app_instance_schema.cal_lead_time(int,int,int) to application role app_instance_role;
@@ -72,6 +72,5 @@ grant usage on procedure app_instance_schema.billing_event(int) to application r
 grant usage on function app_instance_schema.cal_distance(float,float,float,float) to application role app_instance_role;
 grant SELECT on view app_instance_schema.MFG_SHIPPING to application role app_instance_role;
 grant usage on streamlit app_instance_schema.streamlit to application role app_instance_role;
+grant usage on function app_instance_schema.hello_world() to application role app_instance_role;
 grant usage on procedure app_instance_schema.update_reference(string, string, string) to application role app_instance_role;
-
-grant usage on function  app_instance_schema.hello_world() to application role app_instance_role;
